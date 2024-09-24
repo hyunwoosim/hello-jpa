f7d70abf7355b958ada62a375188746d74198ae0
hyunwoo <kshsim98@naver.com>
2024-09-24 17:58:35 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,10 +17,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZZ");
-
+            Member member = new Member(200L, "member200");
             em.persist(member);
+
+            em.flush();
 
             System.out.println("====================");
 
