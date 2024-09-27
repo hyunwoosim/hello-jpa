019cee8eace5b1ca34ac65d0030ddefeeec0c31c
hyunwoo <kshsim98@naver.com>
2024-09-27 19:56:19 +0900

    다대다 [N:M]

--- src/main/java/hellojpa/Product.java
+++ src/main/java/hellojpa/Product.java
@@ -1,37 +1,22 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member {
+public class Product {
 
     @Id @GeneratedValue
-    @Column(name = "MEMBER_ID")
     private Long id;
 
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
-    @Column(name = "USERNAME")
     private String name;
 
-    @OneToOne
-    @JoinColumn(name = "Locker_ID")
-    private Locker locker;
-
-    @ManyToMany
-    @JoinTable(name = "MEMBER_PRODUCT")
-    private List<Product> products = new ArrayList<>();
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
